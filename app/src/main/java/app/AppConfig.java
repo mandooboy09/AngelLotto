d7eccb575d8656ede58b04eb9a3dddf3b1b08302
tysong <mandooboy09@gmail.com>
2018-02-18 17:50:51 +0900

    Initial commit

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by JustKch on 2018-02-12.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://13.125.156.149/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://13.125.156.149/android_login_api/register.php";
+}
